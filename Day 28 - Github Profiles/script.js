d1ea5058bd2c2e230c41621066da153b8294b746
Home <kidusmehretu@gmail.com>
2022-12-21 21:48:38 +0300

    Day 26 Double Vertical Slider

--- Day 28 - Github Profiles/script.js
+++ Day 28 - Github Profiles/script.js
@@ -19,7 +19,7 @@ async function getUser(username){
 
 async function getRepos(username){
     try{
-        const{data}=await axios(APIURL + username + '/repos')
+        const{data}=await axios(APIURL + username + '/repos?sort=created')
 
         addReposToCard(data)
     }catch(err){
@@ -62,6 +62,18 @@ function createErrorCard(msg){
 
 function addReposToCard(repos){
     const reposEl = document.getElementById('repos')
+
+    repos 
+        .slice(0, 5)
+        .forEach(repo => {
+            const repoEl = document.createElement('a')
+            repoEl.classList.add('repo')
+            repoEl.href = repo.html_url
+            repoEl.target = '_blank'
+            repoEl.innerText = repo.name
+
+            reposEl.appendChild(repoEl)
+        })
 }
 
 form.addEventListener('submit', (e)=>{
